c2dfed50f55767387a2b474a8075de0498b73547
Mahmmoud Saleh <Mahmmoud@gmail.com>
2026-04-04 02:36:58 +0300

    Initial commit - JUnit HW

---
+++ test/main/najah/code/TestCalculator.java
@@ -0,0 +1,11 @@
+package main.najah.code;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+
+    @Test
+    void testExample() {
+        System.out.println("WORKING 🔥");
+    }
+}
